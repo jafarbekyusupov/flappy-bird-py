7cb6f8212582af1ad5abb7b319894ca1d1e8debb
Ja'farbek Yusupov <github.jafarbekyusupov@gmail.com>
2025-04-23 13:17:51 +0500

    code readability impoved

--- code/settings.py
+++ code/settings.py
@@ -1,6 +1,5 @@
 import pygame as pg
 
-
 class Settings:
     def __init__(self):
 
@@ -9,25 +8,23 @@ class Settings:
         self.width = 600
         self.height = display_info.current_h - 85
         
-        # ccreen sizes config
+        # screen sizes configurations / options
         self.SCREEN_SIZES = {
             "small": (480, 720),
             "medium": (600, display_info.current_h - 85),
             "large": (720, 1200)
         }
 
-        # init w/ default screen size -- medium
+        # initial default screen size -- medium
         self.current_size = "medium"
 
-        
-
         # game constants / settings
-        self.FPS = 80 # prob gon make it var customizable by user -->> settings dropdown menu
+        self.FPS = 80
         self.speed = 5
         self.gravity = 0.25
         self.pipe_spawn_time = 1300  # ms
         self.bird_flap_time = 200  # ms
-        self.double_click_interval = 0.4  # secs
+        self.double_click_interval = 0.4  # seconds
 
         # colors
         self.WHITE = (255, 255, 255)
@@ -45,7 +42,7 @@ class Settings:
         self.scale_factor = self.width / self.SCREEN_SIZES["medium"][0]
 
     def update_screen_size(self, size):
-        """upd settings when screen size changes"""
+        """update settings when Screen Size changes"""
         if size in self.SCREEN_SIZES:
             self.current_size = size
             self.width, self.height = self.SCREEN_SIZES[size]
